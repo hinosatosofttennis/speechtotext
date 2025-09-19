105e052c9bcb01cfd0a4d25ca700e9bc61b2f1f6
日の里ソフトテニス倶楽部 <hinosatosofttennis@gmail.com>
2025-09-19 23:21:30 +0900

    Update speech-to-text.js
    Api

--- speech-to-text.js
+++ speech-to-text.js
@@ -1,8 +1,8 @@
-// api/speech-to-text.js
+// speech-to-text.js
 export default async function handler(req, res) {
   // CORS設定
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://yourusername.github.io'); // 実際のドメインに変更
+  res.setHeader('Access-Control-Allow-Origin', 'https://hinosatosofttennis.github.io'); // 実際のドメインに変更
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
